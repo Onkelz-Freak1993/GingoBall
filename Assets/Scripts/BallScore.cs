fab71670e10f27e631bbf546b61bcf315ba1b8df
Onkelz-Freak1993 <32557218+Onkelz-Freak1993@users.noreply.github.com>
2018-07-01 16:17:22 +0200

    Added Ball Scoring

---
+++ Assets/Scripts/BallScore.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScore : MonoBehaviour {
+
+public GameObject ObjectToDestroy;
+public GameObject TheObject;
+public GameObject Player;
+
+void OnCollisionEnter(Collision col)    {
+    if (col.gameObject == TheObject) {
+        ObjectToDestroy.SetActive (false);
+    }
+}
+}
