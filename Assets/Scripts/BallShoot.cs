ac998483715c1a08c7f672dbd2cebd1a19bbc488
Onkelz-Freak1993 <32557218+Onkelz-Freak1993@users.noreply.github.com>
2018-07-01 14:58:36 +0200

    Initial Upload

---
+++ Assets/Scripts/BallShoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShoot : MonoBehaviour {
+	
+	public int speedMulti = 25;
+	private float dist;
+     void Update () 
+     {
+		
+		Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+		Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
+		dist = Vector2.Distance (mouseOnScreen, positionOnScreen);
+
+		Debug.DrawLine(transform.position,transform.forward * dist*10 + transform.position, Color.red);
+
+		if (Input.GetMouseButtonDown(0))
+			gameObject.GetComponent<Rigidbody>().velocity = transform.forward * dist * speedMulti;
+        	float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+			transform.rotation =  Quaternion.Euler (new Vector3(0f,90-angle,0f));
+     }
+ 
+     float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+     }
+}
