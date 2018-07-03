c93c6823c757bbd95b727052d7f2722d08296a73
Gingolingoo <32557246+Gingolingoo@users.noreply.github.com>
2018-07-03 09:22:10 +0200

    -

--- Assets/Scripts/BallShoot.cs
+++ Assets/Scripts/BallShoot.cs
@@ -6,27 +6,33 @@ public class BallShoot : MonoBehaviour {
 	public Texture2D shootBG;
 	public Texture2D shootFG;
 	public int speedMulti = 25;
+	public float power = 5;
 
-
+	private bool mouselook = true;
 	private float dist;
+	private Vector2 mouseOnScreen;
+	private Vector2 positionOnScreen;
+
      void Update () 
      {
-		
-		Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-		Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
-		dist = Vector2.Distance (mouseOnScreen, positionOnScreen);
-
-		if (Input.GetMouseButtonDown(0))
-			gameObject.GetComponent<Rigidbody>().velocity = transform.forward * dist * speedMulti;
-        	float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+		if (mouselook) {
+			mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint (Input.mousePosition);
+			positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
+			dist = Vector2.Distance (mouseOnScreen, positionOnScreen);
+			float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
 			transform.rotation =  Quaternion.Euler (new Vector3(0f,90-angle,0f));
-     }
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+			mouselook = false;
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			gameObject.GetComponent<Rigidbody> ().velocity = transform.forward * dist * speedMulti;
+			mouselook = true;
+		}
+	}
  
      float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
-         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+     	return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
      }
-
-	void OnGUI(){
-
-	}
 }
